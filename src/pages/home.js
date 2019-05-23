465fe33a69976a906ba0c16caaddf2cdcc86489d
RamonJul <ramoncarlosjulian@gmail.com>
2019-05-22 21:24:28 -0400

    quick fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,13 @@
 import React ,{Component} from 'react';
 import Main from '../components/mainBar/index';
-import Container from './../components/container/index';
+
 class Home extends Component{
 
      render(){
 
         return(
             <Main>
-               <h2>Hello I am Ramon</h2>
+               <h2>Hello I am Ramon Julian</h2>
 
             </Main>
 
